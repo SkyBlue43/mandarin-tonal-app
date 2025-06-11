4ff7929c94de72fa7dec5d31c79ed8cebe9bccb5
IanLee0724 <ssmm0724@gamil.com>
2025-06-10 21:18:49 -0600

    Replay2

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -117,7 +117,7 @@ export default function Home() {
       <RotateCcw />
     </button>
   )}
-        
+        {audioURL && <audio controls src={audioURL} />}
         </div>
 
         <div>{userPitch.length > 0 && (
