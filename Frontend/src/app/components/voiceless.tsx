1fb7c52672f9a286e83a09f711aad8debd09fac8
Skyler <SkyBlue43@users.noreply.github.com>
2025-06-23 10:45:42 -0600

    Added a function to get MFA data

--- Frontend/src/app/components/voiceless.tsx
+++ Frontend/src/app/components/voiceless.tsx
@@ -64,6 +64,19 @@ export default function Voiceless({
     return data
   };
 
+  const getDataMFA = async (audio_blob: Blob, audio_location: string, audio_text: string) => {
+    const formData = new FormData();
+    formData.append('file', audio_blob, audio_location);
+    formData.append('transcript', audio_text);
+    const result = await fetch('http://localhost:8000/mfa', {
+      method: 'POST',
+      body: formData,
+    });
+    const data = await result.json();
+    console.log("MFA data:", data);
+    return data
+  };
+
 
   function countMatches(aligned: any[], tolerance = 15): number {
     let totalPoints = aligned.length;
